c73d5554e1d0f3c8598c7ccf2d3cf22f481cbe37
Aman <rumman.djuice1@gmail.com>
2019-06-26 20:11:09 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {ContexProvider} from './contex';
+import { createMuiTheme } from '@material-ui/core/styles';//to apply theme
+import { ThemeProvider } from '@material-ui/styles';//Need to warp the <App/> with it like Redux
 
-ReactDOM.render(<ContexProvider><App /></ContexProvider>, document.getElementById('root'));
+import purple from '@material-ui/core/colors/purple';
+const theme=createMuiTheme({ //Changing the default properties of the theme
+    palette:{
+        primary:purple,
+    }
+});
+
+//console.log(theme)
+
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <ContexProvider>
+            <App />
+        </ContexProvider>
+    </ThemeProvider>
+   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
